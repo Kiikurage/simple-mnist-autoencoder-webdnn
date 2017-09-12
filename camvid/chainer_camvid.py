519e0478612cfa9a78b7c47dd8b69576e4434d3a
NOKUBI Takatsugu <knok@daionet.gr.jp>
2017-09-12 10:03:03 +0900

    work trainer

--- camvid/chainer_camvid.py
+++ camvid/chainer_camvid.py
@@ -13,6 +13,7 @@ from chainer.training import extensions
 
 from chainercv.datasets import CamVidDataset
 from chainercv.utils import read_image
+from chainer import cuda
 
 class MyCamVidDataset(CamVidDataset):
     def __init__(self, **kwargs):
@@ -47,7 +48,7 @@ class CamVidAutoEncoder(chainer.Chain):
             self.conv1 = L.Convolution2D(self.channel, 16, ksize=3)
             self.conv2 = L.Convolution2D(16, 32, ksize=3)
             self.deconv1 = L.Deconvolution2D(32, 16, ksize=3)
-            self.deconv1 = L.Deconvolution2D(16, self.channel, ksize=3)
+            self.deconv2 = L.Deconvolution2D(16, self.channel, ksize=3)
 
     def predict(self, x):
         h = self.conv1(x)
@@ -70,15 +71,15 @@ class CamViidAEIterator(chainer.dataset.Iterator):
     def __init__(self, dataset, batch_size):
         self.dataset = dataset
         self.batch_size = batch_size
-        self.pos = -batch_size
+        self.pos = 0
         self.epoch = 0
 
     def fetch(self):
-        self.pos += self.batch_size
         data = []
         try:
             for i in range(self.batch_size):
                 data.append(self.dataset.next())
+            self.pos += self.batch_size
         except:
             self.pos = 0
             self.epoch += 1
@@ -90,21 +91,45 @@ class CamViidAEIterator(chainer.dataset.Iterator):
         if len(data) <= 0:
             data = self.fetch()
         data = np.asarray(data)
-        return data, data
+        return data, data.copy()
 
     @property
     def epoch_detail(self):
         ed = self.epoch + float(self.pos / len(self.dataset))
         return ed
 
+def convert(batch, device):
+    if device >= 0:
+        x = cuda.to_gpu(batch[0])
+        t = cuda.to_gpu(batch[1])
+        return x, t
+    return batch
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('--batchsize', type=int, default=12)
     parser.add_argument('--out', type=str, default='out')
+    parser.add_argument('--epoch', type=int, default=100)
+    args = parser.parse_args()
+
+    # trigger
+    log_trigger = (50, 'iteration')
 
     train = MyCamVidDataset(split='train') # shape (3, 360, 480)
-    import pdb; pdb.set_trace()
+    train_iter = CamViidAEIterator(train, args.batchsize)
+    model = CamVidAutoEncoder(F.mean_squared_error)
+    opt = chainer.optimizers.MomentumSGD()
+    opt.setup(model)
+    updater = training.StandardUpdater(train_iter, opt, converter=convert,
+                                       device=args.gpu)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'))
+    trainer.extend(extensions.LogReport(trigger=log_trigger))
+    trainer.extend(extensions.PrintReport(['epoch', 'loss']), trigger=log_trigger)
+    trainer.extend(extensions.ProgressBar(update_interval=10))
+    trainer.run()
+    chainer.serializers.save_npz("model.npz", model)
+
 
 if __name__ == '__main__':
     main()
